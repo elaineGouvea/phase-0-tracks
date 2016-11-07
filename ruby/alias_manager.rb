291da19d67548ad2b9ca3d7a83907df8d20addc7
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-07 01:08:31 -0800

    Add interface

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -9,13 +9,15 @@
 def change_char(char)
 	alphabet = 'abcdefghijklmnopqrstuvwxyz'
 	vowels = 'aeiou'
+	vowels_list = vowels.split('')
 	char = char.downcase
-	consonants = alphabet.split('') - vowels.split('')
+	consonants = alphabet.split('') - vowels_list
 
 	if !alphabet.index(char)
 		' '
 	elsif vowels.include? char
-		vowels[vowels.index(char) + 1]
+		position = vowels.index(char)
+		return vowels.split('').rotate(1)[position]
 	else 
 		consonants[consonants.index(char) + 1]
 	end
@@ -28,15 +30,32 @@ def alias_generator(name)
 		alias_name << change_char(name[char_index])
 		char_index += 1
 	end
-	alias_name.split(' ')[0].capitalize + ' ' + alias_name.split(' ')[1].capitalize
+	if alias_name.include?(" ") then
+		# alias_name.split(' ')[0].capitalize + ' ' + alias_name.split(' ')[1].capitalize
+		alias_name.split(' ').map { |name| name.capitalize  }.join(' ')
+	else
+		alias_name.split(' ')[0].capitalize
+	end
 end
 
-full_name = "Felicia Torres"
-split_name = full_name.split(' ')
-invert_name = split_name.reverse.join(' ')
-p invert_name
+# full_name = "Felicia Torres"
+# split_name = full_name.split(' ')
+# invert_name = split_name.reverse.join(' ')
+# p invert_name
 
-puts alias_generator(invert_name)
+# Interface
+
+loop do 
+	puts "Welcome to the Alias Generator! Enter your full name and we'll return a cool fake one. Type 'quit' when done."
+	input = gets.chomp
+	split_name = input.split(' ')
+	invert_name = split_name.reverse.join(' ')
+	puts alias_generator(invert_name)
+	break if input == 'quit'
+end
+# p invert_name
+
+
 
 #Tests:
 # puts "Should return a space if input is not a letter:"
