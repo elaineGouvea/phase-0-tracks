515f024e34abd30108090ec4564500acf07a42fe
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-07 22:03:29 -0800

    Add hash to store input and fake names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,10 +1,19 @@
 # Alias Manager
 # Takes a spy real name and create an alias.
-# - Split a 'full_name'string in two: first_name & last-name;
-# - Swap the order of the names: last_name & first-name;
-# - Separate the names into characters;
+# - Create a variable to split a 'full_name'string in an array with 2 strings: first_name & last-name;
+# - Swap the order of the names: last_name & first-name. Create a variable to join the values and store it;
+# - Create a loop to iterate into each character;
+# - Change all the characters to lowercase;
+# - Detect the characters that are not letters;
 # - Detect vowels;
-# - Change each vowel to the next one ('u' should be 'a');
+# - Change each vowel to the next one ('u' becomes 'a');
+# - Detect consonants;
+# - Change each consonants to the next consonant ('d' becomes 'f, 'z' becomes 'b');
+# - Store all the changings into a new variable;
+# - Capitalize the first letter of each word of this variable.
+# 	-
+
+agents = {}
 
 def change_char(char)
 	alphabet = 'abcdefghijklmnopqrstuvwxyz'
@@ -17,26 +26,30 @@ def change_char(char)
 		' '
 	elsif vowels.include? char
 		position = vowels.index(char)
-		return vowels.split('').rotate(1)[position]
+		return vowels_list.rotate(1)[position]
 	else 
-		consonants[consonants.index(char) + 1]
+		consonants.rotate(1)[consonants.index(char)]
 	end
 end
 
-def alias_generator(name)
+def alias_generator(name, agents, input)
 	char_index = 0
 	alias_name = ''
+
 	while char_index < name.length
 		alias_name << change_char(name[char_index])
 		char_index += 1
 	end
-	if alias_name.include?(" ") then
+	if alias_name.include?(' ') then
 		# alias_name.split(' ')[0].capitalize + ' ' + alias_name.split(' ')[1].capitalize
+		# doesn't work for one-word strings!
 		alias_name.split(' ').map { |name| name.capitalize  }.join(' ')
 	else
 		alias_name.split(' ')[0].capitalize
 	end
+	agents[input] = alias_name
 end
+
 
 # full_name = "Felicia Torres"
 # split_name = full_name.split(' ')
@@ -45,15 +58,25 @@ end
 
 # Interface
 
+
 loop do 
 	puts "Welcome to the Alias Generator! Enter your full name and we'll return a cool fake one. Type 'quit' when done."
 	input = gets.chomp
+	break if input == 'quit'
 	split_name = input.split(' ')
 	invert_name = split_name.reverse.join(' ')
-	puts alias_generator(invert_name)
-	break if input == 'quit'
+	puts alias_generator(invert_name, agents, input)
 end
-# p invert_name
+
+agents.each do |name, nickname|
+  puts "#{name} is also known as #{nickname}"
+end
+
+# puts #{agent[0]} is also known as #{agent[1]}
+
+
+
+
 
 
 
@@ -63,3 +86,6 @@ end
 
 # puts "Should return the next vowel if input contains vowels:"
 # p next_vowel("passeio")
+
+# p invert_name
+
