03c17ccb090cde0ed5d762ed5e87d20d338562d7
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-12 13:48:54 -0800

    Resolve second merge conflict

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -32,8 +32,8 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
+# => 122
 # (What is the significance of the number 122 in relation to the character z?)
 
-# "How many times does the letter 'a' appear in this string?".<???>
-# => 4
+"How many times does the letter 'a' appear in this string?".count ("a")
+=> 4
