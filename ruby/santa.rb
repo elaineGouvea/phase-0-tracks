94dadcc60758a36eb72b6f1b567feb6ed6481752
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-12 22:45:21 -0800

    Add 50 Santa instances

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,7 @@
 class	Santa
+	attr_reader :age, :ethnicity
+	attr_accessor :gender
+
 	def initialize(gender,ethnicity)
 		puts "Initializing Santa instance ..."
 		@gender = gender
@@ -14,41 +17,47 @@ class	Santa
 	def eat_milk_and_cookies(cookie)
 		puts "That was a good #{cookie}!"
 	end
+
+	def celebrate_birthday
+		@age = @age + 1
+	end
+
+	def get_mad_at(reindeer_name)
+		reindeer_position = @reindeer_ranking.index(reindeer_name)
+		bad_reindeer = @reindeer_ranking.slice!(reindeer_position)
+		return @reindeer_ranking << bad_reindeer
+	end
 end
 
-# santa = Santa.new("female","Latino")
-# santa.speak
-# santa.eat_milk_and_cookies("oreo")
+# Driver code
+santa = Santa.new("female","Latina")
+puts "Should return reindeer_position (index)"
+# puts santa.get_mad_at("Dancer") == 2
+# puts santa.get_mad_at("Dancer") == "Dancer"
+puts santa.get_mad_at("Dancer")[-1] == "Dancer"
 
-# santas = []
-# p santas << Santa.new("agender", "black")
-# p santas << Santa.new("female", "Latino")
-# p santas << Santa.new("bigender", "white")
-# p santas << Santa.new("male", "Japanese")
-# p santas << Santa.new("female", "prefer not to say")
-# p santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
-# p santas << Santa.new("N/A", "N/A")
+puts "should age santa in one year"
+puts santa.celebrate_birthday
 
-#Driver code
-santas = []
+puts "Should allow change gender outside the method"
+santa.gender = "bigender"
+puts santa.gender == "bigender"
+
+puts "Should allow to return age outside the method"
+puts santa.age == 1
+
+puts "Should allow to return ethnicity outside the method"
+puts santa.ethnicity == "Latina"
+
+#Release 4 - create Lots of Santas
+
 example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-# example_cookies =["oreo", "cookie", "buttermilk"]
-example_genders.length.times do |i|
- puts santas << Santa.new(example_genders[i], example_ethnicities[i])
- puts "There are now #{santas.length} Santas instances in the array"
-end
-
-puts "Testing each Santa to see if we have a great diverse group..."
-
-santas.each do |santa|
-	santa.speak
+50.times do 
+  new_santa = Santa.new(example_genders.sample, example_ethnicities.sample)
+  puts new_santa.speak
 end
 
 
-# example_cookies.length.times do |i|
-# 	puts santas <<
-# santas.each do |santa|
-# 	santa.eat_milk_and_cookies(example_cookies)
-# end
-
+# santa.speak
+# santa.eat_milk_and_cookies("oreo")
