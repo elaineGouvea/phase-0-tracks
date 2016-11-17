f3f07b2f1e4151c87d1a68503081d60c057b7908
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-16 19:32:23 -0800

    Add constructor function

--- js/data_structures.js
+++ js/data_structures.js
@@ -11,7 +11,27 @@ for (var i = 0; i < names.length; i++) {
 	horses['color'] = colors[i];
 
 
-console.log("The horse's name is " + horses['name'] + ".");
-console.log("The horse's color is " + horses.color + ".");
-console.log("_______________________________");
+	console.log("The horse's name is " + horses['name'] + ".");
+	console.log("The horse's color is " + horses.color + ".");
+	console.log("_______________________________");
 }
+
+function car(year, color, brand){
+	this.year = year;
+	this.color = color;
+	this.brand = brand;
+
+	this.accelerate = function() {console.log("Vrooom! Vrooom!")};
+	this.honk = function() {console.log("Beep! Beep!")};
+}
+
+var oldCar = new car (1950, "red", "Ford Mustang");
+var smallCar = new car (2012, "light green", "Fiat 500");
+
+console.log("This " + oldCar.brand + " car is from " + oldCar.year);
+oldCar.accelerate();
+oldCar.honk();
+
+console.log("This " + smallCar.color + " " + smallCar.brand + "car is from " + smallCar.year);
+smallCar.accelerate();
+smallCar.honk();
