efd877a01f3bb65f59e5f32932ff75b199a687c6
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-18 23:02:27 -0800

    Create Pseudocode

---
+++ js/explore.js
@@ -0,0 +1,7 @@
+// Pseudocode - Reverse a string Algorithm
+// - Create a function that takes a string as a parameter
+// - Create an empty array
+// - For each character of the string, iterate through the string length to change the index to a negative one, starting from 0 that becomes -1, 1 that becomes -2, etc
+// - Put the reversed string into the new array
+// - Join the array into a string again and returns its value
+
