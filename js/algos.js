d15c65b81d31a6462f2fdf5fa76ec040eb0008d5
Elaine Gouvea <elaine.gouvea@gmail.com>
2016-11-20 23:53:23 -0800

    Add function to find key-value match

--- js/algos.js
+++ js/algos.js
@@ -1,3 +1,4 @@
+// Business Logic
 // Function to find th longest element in an array
 function longestPhrase(phraseList){
 	// create a variable to store the max length
@@ -17,9 +18,24 @@ function longestPhrase(phraseList){
 	return longestString;
 }
 
+// Function to find a Key-Value Match
+function matchFinder(collection1, collection2){
+	/*use the bracket notation to access both variable and strings
+	iterate through each element of the object to search for equal key-value pairs*/ 
+	return Object.keys(collection1).reduce(function(map, key)) {
+		if (collection1[key] === collection2[key]) map[key] =collection2[k];
+	}
+}
+
+
 // Driver Code:
-// call the function
+// call longestPhrase function
 console.log(longestPhrase(["long phrase","longest phrase","longer phrase"]));
 // create an array to dry invoke function
 var phraseList = ["this is a phrase","This is suppose to be the longest phrase","This phrase is longer than the first"];
 console.log(longestPhrase(phraseList));
+
+// invoke matchFinder function
+var obj1 = {name: "Steven", age: 54};
+var obj2 = {name: "Tamir", age: 54};
+console.log(matchFinder(obj1, obj2));
