29fdf96b9d959dfcf444f5d76bf8d1227330a0c4
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-01-10 18:36:10 -0800

    Hot fix: make vampire detection result be evaluated according to the latest condition matched

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -2,10 +2,10 @@
 
 require 'date' #import date method
 
-def survey_session(times)
+def survey_session(times) #Method to process nultiple employees - Release 3
 	counter = 0
-	while counter < times
-		survey_form
+	while counter < times # Loop to call survey_form according to times
+		survey_form # Call the method survey_form
 	counter +=1
 	end
 	times
@@ -15,7 +15,7 @@ def survey_form
 	puts "What's your name?"
 	employee_name = gets.chomp
 
-# To compare the answers to see if the informed_age matches the informed_birth:
+	# To compare the answers to see if the informed_age matches the informed_birth:
 	puts "What will be your age at the last day of this year?"
 	informed_age = gets.chomp.to_i #convert input to integer
 
@@ -24,91 +24,108 @@ def survey_form
 	age = Date.today.year - informed_birth #get current year 
 
 	puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
-	wants_garlic = gets.chomp.downcase
+	wants_garlic = nil # 1.initialize variable to use on loop
 
-	puts "Would you like to enroll in the company’s health insurance? (y/n)"
-	wants_health_insurance = gets.chomp.downcase
-
-	puts "Name any allergies, one at a time. You should type “done” when finished."
-	allergy = ""
-	is_vampire_detected = false
-	until allergy == "done"
-		allergy = gets.chomp.downcase
-			if allergy == "sunshine"
-				puts "Probably a vampire." 
-				is_vampire_detected = true
-			end
+	# Create loop to check if input is valid
+	until wants_garlic == 'y' || wants_garlic == 'n'
+		wants_garlic = gets.chomp.downcase #use .downcase to make input "case insensitive"
+		if wants_garlic == 'y' 
+			garlic = true #boolean to validate conditions
+		elsif wants_garlic == 'n'
+			garlic = false
+		else
+			puts 'Option invalid! Please, answer with "y" or "n".' #Alert msg.
+		end
 	end
 
-	if !is_vampire_detected
+	p garlic
 
+	puts "Would you like to enroll in the company’s health insurance? (y/n)"
+	wants_health_insurance = nil
+		
+	until wants_health_insurance == 'y' || wants_health_insurance == 'n'	
+		wants_health_insurance = gets.chomp.downcase
+		if wants_health_insurance == 'y' 
+			health_insurance = true
+		elsif wants_health_insurance == 'n'
+			health_insurance = false
+		else
+			puts 'Option invalid! Please, answer with "y" or "n".'
+		end
+	end
+ 	 
+	# Release 4: Check Allergies
+	puts "Name any allergies, one at a time. You should type “done” when finished."
+	allergy = nil
+	is_vampire_detected = false # Boolean to check if vampire is detected through suspect allergies.
+
+	# Loop to name allergies, break if "done". If the allergy is "sunshine", skip to the result of "Probably a vampire."
+	until allergy == "done"
+		allergy = gets.chomp.downcase
+		if allergy == "sunshine"
+			is_vampire_detected = true
+		end
+	end
+
+	# Detect a vampire through allergies
+	if is_vampire_detected 
+		puts "Probably a vampire." 
+	end
+
+	# Case vampire is not detected through allergies:
+	if !is_vampire_detected 
+		# Compare to see if informed age is equal actual age
 		if informed_age == age
 			is_age_correct = true
 		else 
 			is_age_correct = false
 		end
 
-		if wants_garlic == "y" 
-			garlic = true
-		else 
-			garlic = false
-		end
-
-		if wants_health_insurance == "y" 
-			health_insurance = true
-		else 
-			health_insurance = false
-		end
-
 		#Vampire Detecting logic:
 
-		vampire_detection = nil #variable to be updated so that the result can be based on the latest condition matched.
-		vampire_level = ''
-		#simplifying the read:
-		garlic_or_insurance = garlic || health_insurance 
-		garlic_and_insurance = garlic && health_insurance
+		vampire_level = nil #variable to be updated so that the result can be based on the latest condition matched.
 
-
-		#Could not find how to evaluate the result based on the latest condition matched instead of the first! I tried to use 2 variables to update them as each condition is checked but it didn't work...
-		 
-		if is_age_correct && garlic_or_insurance
-			vampire_detection = false
+		#Evaluating the conditions:
+		if is_age_correct && ( garlic || health_insurance )
 			vampire_level = 0
-		elsif !is_age_correct && !garlic_or_insurance
-			vampire_detection = false
+		end
+
+		if !is_age_correct && ( !garlic || !health_insurance )
 			vampire_level = 1
-		elsif !is_age_correct && !garlic_and_insurance
-			vampire_detection = false
+		end
+
+		if !is_age_correct && !garlic && !health_insurance
 			vampire_level = 2
-		elsif employee_name == "Drake Cula" || "Tu Fang"
-			vampire_detection = true
-		else 
-			vampire_detection = false
+		end
+
+		if employee_name == "Drake Cula" || employee_name == "Tu Fang"
 			vampire_level = 3
 		end
-
-		if vampire_detection == false 
-			if vampire_level == 0
+	
+		#Analyzing Vampire Detection result through vampire_level:
+		if vampire_level == 0
 			puts "Probably not a vampire."
-			elsif vampire_level == 1
-				puts "Probably a vampire."
-			elsif vampire_level == 2
-				puts "Almost certainly a vampire."
-			else vampire_level == 3
-				puts "Result inconclusive."
-			end
-		else
+		elsif vampire_level == 1
+			puts "Probably a vampire."
+		elsif vampire_level == 2
+			puts "Almost certainly a vampire."
+		elsif vampire_level == 3
 			puts "Definitely a vampire."
+		else
+			puts "Result inconclusive."
 		end
 	end
+	puts "====================================================="
 end
 
+# Release 3: Process Multiple Employees
 puts "How many employees will be processed?"
 number_employees = gets.chomp
 
 times = number_employees.to_i
 
-survey_session(times)
+survey_session(times) #call the method survey_session, passing the argument of times 
 
+# Release 5: Plot Twist
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
 
