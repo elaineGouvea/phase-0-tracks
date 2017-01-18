f5c4500373abe332edaad218c3cedf6ea1d8bbce
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-01-17 16:26:13 -0800

    Add Puppy class

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,69 +1,37 @@
+# Release 0
+
 class Puppy
-  def initialize
-		puts "Initializing new puppy instance..."
-	end
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
     toy
   end
 
-	def speak(bark_number)
-		i = 0
-		until i == bark_number
-			puts "Woof!"
-		i += 1
-		end
-	end  
+  def speak(number)
+  	(number).times do |i|
+  	puts "Woof!"
+  	end 
+  end
 
-	def roll_over
-		puts '*rolls over*'
-	end
+  def roll_over
+  	puts "*rolls over*"
+  end
 
-	def dog_years(human_years)
-		puts human_years * 7
-	end
+  def dog_years(human_age)
+  	puts human_age * 7 
+  end
 
-	def hand_shake
-		puts "*gives paw*"
-	end
+  def sit
+  	puts "*sitting*"
+  end
 end
 
-# fido = Puppy.new
+# DRIVER CODE
 
-# fido.fetch("ball")
-
-# fido.speak(3)
-
-# fido.roll_over
-
-# fido.dog_years(6)
-
-# fido.hand_shake
-
-class Kitten
-	def initialize
-		puts "Initializing new Kitten instance..."
-	end
-
-	def cuddle
-		puts "*cuddles legs*"
-	end
-
-	def cat_years(human_years)
-		puts human_years * 7
-	end
-end
-
-cat_house = []
-
-i = 0
-
-until i == 50 
-	cat_house << Kitten.new
-	i += 1
-end
-
-cat_house.each {|kitten| kitten.cuddle}
-
+fido = Puppy.new
+fido.fetch("bone")
+fido.speak(6)
+fido.roll_over
+fido.dog_years(8)
+fido.sit
 
