b1978f87e738fd0ba55ba4bf2367e85e10051a17
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-01-21 16:56:57 -0800

    Add initialize, speak and eat_milk_and_cookies to Santa class

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -3,10 +3,9 @@
 # BUSINESS LOGIC
 class	Santa
 
-	def initialize(gender, ethnicity)
+	def initialize
 		puts "Initializing Santa instance ..."
-		reindeers = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		
 	end
 
 	def speak
@@ -20,28 +19,13 @@ end
 
 # DRIVER CODE
 
-# Release 1 tests:
-santas = []
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_genders.length.times do |i|
-  santas << Santa.new(example_genders[i], example_ethnicities[i])
-end
-
-puts "Should return each Santa instance in the array executing its methods"
-santas.each do |santa|
-	santa.speak
-	santa.eat_milk_and_cookies('oreo')
-end
-
-
 # Release 0 tests:
-# puts "Should initialize a Santa instance."
-# santa = Santa.new
-
-# puts "Call speak method should return a greeting"
-# santa.speak
-
-# puts "Call eat_milk_and_cookies method passing 'oreo' as a parameter should return a greeting using the argument"
-# santa.eat_milk_and_cookies("oreo")
+puts "Should initialize a Santa instance:"
+santa = Santa.new
+puts
+puts "Call speak method should return a greeting:"
+santa.speak
+puts
+puts "Call eat_milk_and_cookies method passing 'snickerdoodle' as a parameter should return a greeting using the argument:"
+santa.eat_milk_and_cookies("snickerdoodle")
 
