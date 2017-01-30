03a072942a85e313f8eb5dd98128da4f124151fe
Elaine Gouvea <elaine.gouvea@gmail.com>
2017-01-29 23:01:14 -0800

    Add matchFinder function

--- js/algos.js
+++ js/algos.js
@@ -1,41 +1,59 @@
+// Find the longest phrase:
 // Business Logic
-// Function to find th longest element in an array
-function longestPhrase(phraseList){
-	// create a variable to store the max length
-	var maxLength = 0;
-	// create a variable to return the max length element value
-	var longestString;
-	// itetare through each element of the array 
-	for (var i = 0; i < phraseList.length; i++) {
-		// set a condition to compare elements length 
-		if (phraseList[i].length > maxLength) {
-			// assign to maxLength the highest length
-			var maxLength = phraseList[i].length; 
-			// assign to longestString the maxLength item
-			var longestString = phraseList[i];
-		}
-	}
-	return longestString;
+// Takes an array of phrases and return the longest:
+function lengthComparator(phrases) {
+	// sets an array to store each phrase length
+	var phrasesLength = [];
+	// var to store the highest number of phrasesLength:
+	var max;
+	// var to store the index of max:
+	var idx;
+	// sets a variable to store the longest phrase
+	var longestPhrase = '';
+	// while index <= phrases array length
+	for (var i = 0; i < phrases.length; i++) {
+		// gets the length of each item of the phrases array
+		phrasesLength.push(phrases[i].length);
+  }
+  max = Math.max.apply(null,phrasesLength);
+  idx = phrasesLength.indexOf(max);
+  return phrases[idx];
 }
 
-// Function to find a Key-Value Match
+// Finds a Key-Value Match
 function matchFinder(collection1, collection2){
-	/*use the bracket notation to access both variable and strings
-	iterate through each element of the object to search for equal key-value pairs*/ 
-	return Object.keys(collection1).reduce(function(map, key)) {
-		if (collection1[key] === collection2[key]) map[key] =collection2[k];
+	if (collection1.name === collection2.name) {
+		return true;
 	}
+	if (collection1.age === collection2.age) {
+		return true;
+	}
+	return false;
 }
 
 
-// Driver Code:
-// call longestPhrase function
-console.log(longestPhrase(["long phrase","longest phrase","longer phrase"]));
-// create an array to dry invoke function
-var phraseList = ["this is a phrase","This is suppose to be the longest phrase","This phrase is longer than the first"];
-console.log(longestPhrase(phraseList));
-
-// invoke matchFinder function
+// Driver Code
+// Release 1
 var obj1 = {name: "Steven", age: 54};
 var obj2 = {name: "Tamir", age: 54};
+console.log('expect matchFinder(obj1, obj2) to return true')
 console.log(matchFinder(obj1, obj2));
+
+var obj3 = {name: "John", age: 6};
+var obj4 = {name: "Mary", age: 98};
+console.log('expect matchFinder(obj3, obj4) to return false')
+console.log(matchFinder(obj3, obj4));
+
+// Release 0
+phrasesArr = ["long phrase","longest phrase","longer phrase"];
+var result = lengthComparator(phrasesArr);
+
+console.log('expect longest phrase to be longest phrase:');
+console.log(result)
+
+wordsArr = ["ball", "car", "elevator", "telephone"]
+var result2 = lengthComparator(wordsArr);
+console.log('expect longest word to be telephone:');
+console.log(result2)
+
+
